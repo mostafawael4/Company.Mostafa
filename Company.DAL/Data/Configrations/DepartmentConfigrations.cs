b879a1e819af42bfb91180df16f2008a34ff2a2b
mostafa wael <mw038818@gmail.com>
2025-03-08 21:19:57 +0200

    implemintation of DAL

---
+++ Company.DAL/Data/Configrations/DepartmentConfigrations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Company.DAL.Data.Configrations
+{
+    internal class DepartmentConfigrations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10,10)
+        }
+    }
+}
